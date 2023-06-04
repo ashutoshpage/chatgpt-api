6f2b538d9c94c94a7e25df4005fda4b165a41c79
Ashutosh <124264120+ashutoshpage@users.noreply.github.com>
2023-06-04 20:23:03 +0530

    chatgpt simple.py

--- 01 chatgpt simple.py
+++ 01 chatgpt simple.py
@@ -1,6 +1,6 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "api-key"
 
 completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": "Tell me about Penguins "}])
 print(completion.choices[0].message.content)
